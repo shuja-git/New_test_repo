cae0b2254959adc26acc7091fa852b364088c39a
shuja-git <95127212+shuja-git@users.noreply.github.com>
2022-10-30 14:16:29 +0530

    file added from github

---
+++ main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+  local = {
+      source = "hashicorp/local"
+      version = "2.2.3"
+}
+}
+}
+resource "local_file" "p1" {
+   filename = each.value
+   content = "this is new one"
+   for_each = toset(var.filename)
+}
